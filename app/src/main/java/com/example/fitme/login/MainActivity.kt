a5c76062d73f4eb3a28c4c222f812520866817fd
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2024-06-01 16:21:26 +0700

    feat(login): add page

--- app/src/main/java/com/example/fitme/login/MainActivity.kt
+++ app/src/main/java/com/example/fitme/login/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.fitme
+package com.example.fitme.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fitme.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/example/fitme/signup/SignUpActivity.java
@@ -0,0 +1,4 @@
+package com.example.fitme.signup;
+
+public class SignUpActivity {
+}
