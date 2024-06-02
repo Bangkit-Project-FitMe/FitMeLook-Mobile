d8521dc5c181941122bf014af04c70fd94638a48
tlithaee <syarifah.talithae@gmail.com>
2024-06-02 17:24:47 +0700

    feat: add xml navbar

---
+++ app/src/main/java/com/example/fitme/ui/NavbarActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fitme.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class NavbarActivity: AppCompatActivity() {
+}
---
+++ app/src/main/java/com/example/fitme/ui/ProfileActivity.kt
@@ -0,0 +1,4 @@
+package com.example.fitme.ui
+
+class ProfileActivity {
+}
