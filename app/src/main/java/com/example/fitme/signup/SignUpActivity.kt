2bb585c78c8891a49fe0e5f4e76a86b6914bc6dd
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2024-06-10 01:23:13 +0700

    feat: implement google sign up

--- app/src/main/java/com/example/fitme/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/fitme/signup/SignUpActivity.kt
@@ -3,20 +3,34 @@ package com.example.fitme.signup
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitme.databinding.ActivitySignupBinding
 import com.example.fitme.home.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.example.fitme.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
 
         setupView()
         setupBinding()
@@ -29,6 +43,7 @@ class SignUpActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
     private fun setupView() {
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -48,7 +63,59 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         binding.googleSignupButton.setOnClickListener {
-            Toast.makeText(this, "Google Sign Up", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this, "Google Sign Up", Toast.LENGTH_SHORT).show()
+            signIn()
         }
+    }
+
+    private fun signIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.your_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    updateUI()
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    private fun updateUI() {
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            startActivity(Intent(this@SignUpActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    companion object {
+        private const val TAG = "SignUpActivity"
+        private const val RC_SIGN_IN = 9001
     }
 }
